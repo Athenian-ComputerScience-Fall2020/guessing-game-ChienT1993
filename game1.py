bc2446eebc93bb887cf6d994692f56c0b1ddb556
ChienT1993 <21ctran@athenian.org>
2020-09-17 09:09:51 +0000

    unfinished (need help)

--- game1.py
+++ game1.py
@@ -8,3 +8,29 @@ Use this file to write an "open" version of the game (no test code or defined fo
 "I'm sorry, you are out of guesses."
 
 '''
+import random
+
+random_num= random.randint(0,10)
+
+tries = 0
+
+def user():
+    user_input= int(input("Type your number here: "))
+    return user_input
+
+if user() == random_num:
+    print ("Congratulation, you have guess the correct number!")
+elif user() < random_num:
+    print ("Your number is too low!")
+    tries = tries + 1
+elif user() > random_num:
+    print ("Your number is too high!")
+    tries = tries + 1
+elif user() > 10:
+    print ("Sorry, your number is out of range!")
+    tries = tries + 1
+
+if tries == 5:
+    print ("Game over, the number you are looking for is: " + random_num)
+elif tries > 0:
+    print (user())
