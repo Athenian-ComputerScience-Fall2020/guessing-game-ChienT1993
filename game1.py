18160892e6606a4c35b83f0c5db415a08161c031
ChienT1993 <21ctran@athenian.org>
2020-09-24 21:57:06 +0000

    unfinish

--- game1.py
+++ game1.py
@@ -10,81 +10,47 @@ Use this file to write an "open" version of the game (no test code or defined fo
 
 '''
 import random
+a = user()
+x = int(input("Enter a number where your range of guessing number will start: "))
+y = int(input("Enter a number where your range of guessing number will end: "))
+random_num= random.randint(x,y)
 
-random_num= random.randint(0,10)
+tries= 1
 
-tries = 1
-
+z = int(input("Enter how many guesses you want to do: "))
 def user():
     user_input= int(input("Type your number here: "))
     return user_input
 
-inp1 = user()
 
-if inp1 == random_num:
+def guess():
+ if a == random_num:
     print ("Congratulation, you have guess the correct number!")
     exit ()
-elif inp1 < random_num:
-    print ("Your number is too low!")
-    tries = tries + 1
-    inp2 = user()
-elif inp1 > 10:
-    print ("Sorry, your number is out of range!")
-    tries = tries + 1
-    inp2 = user()
-elif inp1 > random_num:
-    print ("Your number is too high!")
-    tries = tries + 1
-    inp2 = user()
-
-if inp2 == random_num:
-    print ("Congratulation, you have guess the correct number!")
-    exit ()
-elif inp2 < random_num:
-    print ("Your number is too low!")
-    tries = tries + 1
-    inp3 = user()
-elif inp2 > 10:
-    print ("Sorry, your number is out of range!")
-    tries = tries + 1
-    inp3 = user()
-elif inp2 > random_num:
-    print ("Your number is too high!")
-    tries = tries + 1
-    inp3 = user()
-
-if inp3 == random_num:
-    print ("Congratulation, you have guess the correct number!")
-    exit ()
-elif inp3 < random_num:
-    print ("Your number is too low!")
-    tries = tries + 1
-    inp4 = user()
-elif inp3 > 10:
-    print ("Sorry, your number is out of range!")
-    tries = tries + 1
-    inp4 = user()
-elif inp3 > random_num:
-    print ("Your number is too high!")
-    tries = tries + 1
-    inp4 = user()
-
-if inp4 == random_num:
-    print ("Congratulation, you have guess the correct number!")
-    exit ()
-elif inp4 < random_num:
-    print ("Your number is too low!")
-    tries = tries + 1
-    inp5 = user()
-elif inp4 > 10:
-    print ("Sorry, your number is out of range!")
-    tries = tries + 1
-    inp5 = user()
-elif inp4 > random_num:
-    print ("Your number is too high!")
-    tries = tries + 1
-    inp5 = user()
-
-if tries == 5:
+ elif tries == z:
     print ("Game over, you are out of chances!")
     print ("The correct answer is " + str(random_num))
+    exit ()
+ elif a < random_num:
+    print ("Your number is too low!")
+    tries = tries + 1
+    inp = user()
+ elif a > 10:
+    print ("Sorry, your number is out of range!")
+    tries = tries + 1
+    inp = user()
+ elif a > random_num:
+    print ("Your number is too high!")
+    tries = tries + 1
+    inp = user()
+
+yes = 1
+no = 2
+try:
+    again = input("Do you want to try again? (yes or no): ")
+    if again == yes:
+        user()
+    elif again == no:
+        print ("Thanks for spending your time on the game!")
+except:
+    print ("Sorry, please type yes or no")
