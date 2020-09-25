ea8d0ee53f0b449306a7cdb055b99606bd593480
ChienT1993 <21ctran@athenian.org>
2020-09-25 16:24:27 +0000

    unfinish

--- game1.py
+++ game1.py
@@ -10,7 +10,7 @@ Use this file to write an "open" version of the game (no test code or defined fo
 
 '''
 import random
-a = user()
+
 x = int(input("Enter a number where your range of guessing number will start: "))
 y = int(input("Enter a number where your range of guessing number will end: "))
 random_num= random.randint(x,y)
@@ -22,35 +22,27 @@ def user():
     user_input= int(input("Type your number here: "))
     return user_input
 
-
-def guess():
- if a == random_num:
+def guess(tries, inp):
+ if inp == random_num:
     print ("Congratulation, you have guess the correct number!")
     exit ()
  elif tries == z:
     print ("Game over, you are out of chances!")
     print ("The correct answer is " + str(random_num))
     exit ()
- elif a < random_num:
+ elif inp < random_num:
     print ("Your number is too low!")
     tries = tries + 1
     inp = user()
- elif a > 10:
+ elif inp > 10:
     print ("Sorry, your number is out of range!")
     tries = tries + 1
     inp = user()
- elif a > random_num:
+ elif inp > random_num:
     print ("Your number is too high!")
     tries = tries + 1
     inp = user()
 
-yes = 1
-no = 2
-try:
-    again = input("Do you want to try again? (yes or no): ")
-    if again == yes:
-        user()
-    elif again == no:
-        print ("Thanks for spending your time on the game!")
-except:
-    print ("Sorry, please type yes or no")
+inp= user()
+for a in range (1, z):
+    guess(tries, inp)
